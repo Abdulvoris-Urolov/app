19687ce8cb86bed5d0e06b342e1cde495aeacff1
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-10-28 00:32:30 +0500

    classes public

--- features/classes.js
+++ features/classes.js
@@ -1,15 +1,44 @@
-var Vehicle = /** @class */ (function () {
-    function Vehicle() {
-    }
-    Vehicle.prototype.drive = function () {
-        console.log('Abdulvoris');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    ;
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle(color) {
+        this.color = color;
+    }
     Vehicle.prototype.honk = function () {
         console.log(('Urolov'));
     };
     return Vehicle;
 }());
-var vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+var vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Car.prototype.drive = function () {
+        console.log('Abdulvahob');
+    };
+    ;
+    Car.prototype.startDrivingProcess = function () {
+        this.drive();
+        this.honk();
+    };
+    return Car;
+}(Vehicle));
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+var car = new Car('red');
+car.startDrivingProcess();
--- features/classes.ts
+++ features/classes.ts
@@ -1,13 +1,35 @@
 class Vehicle {
-  drive (): void{
-    console.log('Abdulvoris');
-  };
-  honk (): void{
+
+  constructor(public color: string){
+  }
+
+  protected honk (): void{
     console.log(('Urolov'));
     
   }
 }
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+
+class Car extends Vehicle {
+  private drive (): void{
+    console.log('Abdulvahob');
+  };
+
+  constructor(public wheels: number, public  color: string){
+    super(color);
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+// const vehicle = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
