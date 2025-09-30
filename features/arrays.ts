d4a4cc346350f873f7e08a290c8da7bba2f878a3
Abdulvoris <abdulvoris000urolov@gmail.com>
2025-09-30 16:57:55 +0500

    [change]:mapbox

--- features/arrays.ts
+++ features/arrays.ts
@@ -20,4 +20,3 @@ const importDates: (Date | string)[] = [new Date()];
 importDates.push('2023-10-10');
 importDates.push(new Date());
 
-// bu comment faqat test uchun push qilish uchun kerak bo'ldi 
